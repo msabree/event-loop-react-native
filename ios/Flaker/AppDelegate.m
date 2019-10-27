a83adbdabb54815ecee99b88fbf2c989cd3440ff
Makeen Sabree <makeen.sabree@gmail.com>
2019-10-27 13:59:11 -0400

    ReactNativeFirebase v6 added.

--- ios/Flaker/AppDelegate.m
+++ ios/Flaker/AppDelegate.m
@@ -5,6 +5,8 @@
  * LICENSE file in the root directory of this source tree.
  */
 
+@import Firebase;
+
 #import "AppDelegate.h"
 
 #import <React/RCTBridge.h>
@@ -43,6 +45,10 @@ fetchCompletionHandler:(void (^)(UIBackgroundFetchResult))completionHandler
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
+  if ([FIRApp defaultApp] == nil) {
+    [FIRApp configure];
+  }
+
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"Flaker"
